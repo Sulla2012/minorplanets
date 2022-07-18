f75dc197a9497dd24a55dde50565db1b47a187de
John Orlowski-Scherer <jorlo@nia-login06.scinet.local>
2022-07-18 18:17:20 -0400

    modified asteroid move to do array jobs

--- run_stack.sh
+++ run_stack.sh
@@ -3,21 +3,28 @@
 #
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=40
-#SBATCH --time=3:00:00
-#SBATCH --job-name tnos_serialx40
-#SBATCH --output=tnos_1_40__%j.txt
+#SBATCH --time=2:00:00
+#SBATCH --job-name asteroids_serialx40
+#SBATCH --output=asteroids_1_40_%j.txt
 #SBATCH --mail-type=FAIL
 
+module load python/3.8.5
+source '/home/r/rbond/jorlo/actmadcows/bin/activate'
+cd $SLURM_SUBMIT_DIR
 
-source ~/intel-jax/bin/activate
-# Turn off implicit threading in Python, R
+python3.8 ~/dev/minorplanets/asteroid_movie_ricco.py /gpfs/fs0/project/r/rbond/sigurdkn/actpol/ephemerides/objects/Iris.npy iris "/home/r/rbond/sigurdkn/project/actpol/maps/depth1/release/*/*_map.fits" /scratch/r/rbond/jorlo/actxminorplanets/sigurd/asteroids/iris
+
+
+
+
+
 
 
 cd $SLURM_SUBMIT_DIR
 
 iter=$(($SLURM_ARRAY_TASK_ID*40))
 # EXECUTION COMMAND; ampersand off 20 jobs and wait
-task=$(expr $iter + 0) && python3 run_stack.py $task &
+task=$(expr $iter + 0) && name=$(sed -n "${task}p" asteroid_list.txt) && python3.8 ~/dev/minorplanets/asteroid_movie.py /gpfs/fs0/project/r/rbond/sigurdkn/actpol/ephemerides/objects/$name.npy iris "/home/r/rbond/sigurdkn/project/actpol/maps/depth1/release/*/*_map.fits" /scratch/r/rbond/jorlo/actxminorplanets/sigurd/asteroids/$name &
 task=$(expr $iter + 1) && python3 run_stack.py $task &
 task=$(expr $iter + 2) && python3 run_stack.py $task &
 task=$(expr $iter + 3) && python3 run_stack.py $task &
